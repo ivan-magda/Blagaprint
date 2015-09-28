dd446386f568cbcfda46c3ac642a4a60e42f97b3
Ivan Magda <imagda15@gmail.com>
2015-09-28 19:05:08 +0900

    Integrate with GuillotineMenu. Add UICollectionView on preview screen. Initial UI of app.

---
+++ Blagaprint/CategoryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Blagaprint
+//
+//  Created by Ivan Magda on 28.09.15.
+//  Copyright © 2015 Blagaprint. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryImageView: UIImageView?
+    @IBOutlet weak var categoryNameLabel: UILabel!
+
+}
--- Blagaprint/ViewController.swift
+++ Blagaprint/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let kCollectionViewCellReuseIdentifier = "CollectionViewCell"
+    
+    @IBOutlet weak var barButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let categories: [String]! = ["Именные чехлы", "Именные футболки", "Печать на кружках", "Фотопечать", "Копи-услуги"]
+    
+// MARK: - ViewController lifecycle -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let navBar = self.navigationController!.navigationBar
+        navBar.barTintColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        self.collectionView.backgroundColor = UIColor(red: 44.0 / 255.0, green: 42.0 / 255.0, blue: 54.0 / 255, alpha: 1)
     }
 
-
+// MARK: - Navigation -
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "MenuSegue" {
+            let destinationVC = segue.destinationViewController as! GuillotineMenuViewController
+            destinationVC.hostNavigationBarHeight = CGRectGetHeight(self.navigationController!.navigationBar.frame)
+            destinationVC.hostTitleText = self.navigationItem.title
+            destinationVC.view!.backgroundColor = self.navigationController!.navigationBar.barTintColor
+            destinationVC.setMenuButtonWithImage(self.barButton.imageView!.image!)
+        }
+    }
 }
 
+// MARK: - UICollectionViewDataSource -
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: CategoryCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(kCollectionViewCellReuseIdentifier, forIndexPath: indexPath) as! CategoryCollectionViewCell
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        let collectionViewCell: CategoryCollectionViewCell = cell as! CategoryCollectionViewCell
+        
+        collectionViewCell.layer.cornerRadius = 5
+        collectionViewCell.categoryImageView?.image = UIImage(named: "\(indexPath.row).jpg")
+        collectionViewCell.categoryNameLabel.text = categories[indexPath.row]
+    }
+}
+
+// MARK: - UICollectionViewDelegate -
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
